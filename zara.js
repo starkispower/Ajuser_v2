da0a26044ba0e4827ccb550a457eb649adbca774
starkispower <92918007+starkispower@users.noreply.github.com>
2021-10-31 10:13:08 +0530

    Update zara.js

--- zara.js
+++ zara.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+917994873071' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'Aju ser' : process.env.OWNER_NAME,
+ Z_NUMBER: process.env.NUMBER === undefined ? '+919074409995' : process.env.NUMBER,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'STARK SER' : process.env.OWNER_NAME,
 
 };
